17f4723a621f6988c9172e3412851a54c2f1fe69
Jjmesia <jjmesia@mcm.edu.ph>
2024-03-22 14:23:44 +0800

    Made the Header and Home Page

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Web Development", "Database Management", "Robotics"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
